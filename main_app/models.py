d58672da68cf259cb70d3641ca491b8126fde655
alonzoalcantar <pinoa.03@outlook.com>
2022-12-25 00:10:24 -0700

    Session Model appears next to individual character details

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+
 class Character(models.Model):
     name = models.CharField(max_length=100)
     race = models.CharField(max_length=100)
@@ -24,3 +25,12 @@ class Item(models.Model):
 
     def get_absolute_url(self):
         return reverse("itemdetail", kwargs={"item_id": self.id})
+
+class Session(models.Model):
+    date = models.DateField('Session Date')
+    session = models.TextField(max_length=250)
+
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_session_display()} on {self.date}"
